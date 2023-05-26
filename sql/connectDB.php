3dc270a54559d762eb4d283b2c233d766aa2fffd
SvRyDev <116703934+SvRyDev@users.noreply.github.com>
2023-05-26 16:06:45 -0500

    Revert "Elimi"
    This reverts commit b5afc314bbc4b508c9bebc050ea1a7d3700f3697.

--- sql/connectDB.php
+++ sql/connectDB.php
@@ -8,6 +8,22 @@ $dbname = "arrow-shade-3774.defaultdb";
 
 $dsn = "pgsql:host=".$dburl.";port=".$port.";dbname=".$dbname.";user=".$dbuser.";password=".$dbpassword;
 
+// if (!$conn) {
+//     echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+//     echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+//     echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+//     exit;
+// }
+// /*
+// echo "Éxito: Se realizó una conexión apropiada a MySQL! La base de datos mi_bd es genial." . PHP_EOL;
+// echo "Información del host: " . mysqli_get_host_info($conn) . PHP_EOL; */
+
+// /* mysqli_close($conn);*/
+
+
+
+try{
+ // create a PostgreSQL database connection
  $conn = new PDO($dsn);
  
  // display a message if connected to the PostgreSQL successfully
