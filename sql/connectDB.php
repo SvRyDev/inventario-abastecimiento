0fd7d467e7e85052907acb2543fb66d7aa665803
SvRyDev <116703934+SvRyDev@users.noreply.github.com>
2023-05-27 10:58:01 -0500

    Actualizado el Sabado 27/05

--- sql/connectDB.php
+++ sql/connectDB.php
@@ -8,33 +8,14 @@ $dbname = "arrow-shade-3774.defaultdb";
 
 $dsn = "pgsql:host=".$dburl.";port=".$port.";dbname=".$dbname.";user=".$dbuser.";password=".$dbpassword;
 
-// if (!$conn) {
-//     echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
-//     echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
-//     echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
-//     exit;
-// }
-// /*
-// echo "Éxito: Se realizó una conexión apropiada a MySQL! La base de datos mi_bd es genial." . PHP_EOL;
-// echo "Información del host: " . mysqli_get_host_info($conn) . PHP_EOL; */
-
-// /* mysqli_close($conn);*/
-
-
 
 try{
  // create a PostgreSQL database connection
  $conn = new PDO($dsn);
- 
- // display a message if connected to the PostgreSQL successfully
- if($conn){
-    // echo "Connected to the <strong>$dbname</strong> database successfully!";
-    // $query = "select * from producto";
-    // foreach($conn->query($query) as $row){
-    //     echo $row['producto'] . "<br/>"; 
-    // }
- }
+
 } catch (PDOException $e){
  // report error message
  echo $e->getMessage();
 }
+
+?>
